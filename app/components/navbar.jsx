177d8517025a30ff5667668151ec7b3a1f49361c
Mavine1 <mavinemalin@gmail.com>
2025-05-22 23:22:00 +0300

    palace rose point already length

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,12 +1,14 @@
- import Image from 'next/image'
+ import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
  
  const navbar = () => {
    return (
      <>
      <nav>
-        < href="#">
-        <Image
+        <a href="#">
+        <Image src={assets.logo} alt="logo" />
+        </a>
      </nav>
      </>
    )
