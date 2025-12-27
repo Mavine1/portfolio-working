04ee20f7a415cd5a664c0694928423e962b15a01
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2025-12-27 23:04:01 +0300

    vast trunk how stems lamp

---
+++ zipp/app/components/Navbar.jsx
@@ -0,0 +1,26 @@
+ import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+ 
+ const Navbar = () => {
+   return (
+     <>
+     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%]'>
+        <a href="#">
+        <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt="logo" />
+        </a>
+       <ul>
+            <li><a href="#top">Home</a></li>
+            <li> <a href ="#about">About me</a></li>
+            <li><a href="#work">Projects</a></li>
+            <li> <a href="#contact">Contact me </a></li>
+       </ul>
+       <div>
+        <a href="#contact">Contact <Image src={assets.arrow_icon } className='w-3' alt='icon'/></a>
+       </div>
+     </nav>
+     </>
+   )
+ }
+ //navabar docymentation 
+ export default Navbar
